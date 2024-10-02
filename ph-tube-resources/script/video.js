7686eab66c3666b3977b6fe31b319fe60497227c
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-02 23:38:55 +0600

    some practice is done

--- ph-tube-resources/script/video.js
+++ ph-tube-resources/script/video.js
@@ -1,12 +1,58 @@
 // 1: fetch load and show categories on html
 // create load categories
 const loadCategories = () => {
-    //fetch the data
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
-    .catch(error => console.log(error))
-}
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+const loadVideos = () => {
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+// const cardDemo = {
+//     category_id: "1001"
+// ​​
+// description: "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night."
+// ​​
+// others: Object { views: "543K", posted_date: "" }
+// ​​
+// thumbnail: "https://i.ibb.co/QPNzYVy/moonlight.jpg"
+// ​​
+// title: "Midnight Serenade"
+// ​​
+// video_id: "aaab"
+// }
+
+const displayVideos = (videos) => {
+    console.log(videos)
+  const videosContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.classList = 'card card-compact'
+    card.innerHTML = `
+        <figure>
+    <img
+      src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `;
+        videosContainer.appendChild(card)
+  });
+};
 
 // {
 //     "category_id": "1001",
@@ -15,17 +61,18 @@ const loadCategories = () => {
 
 // create display categories
 const displayCategories = (categories) => {
-    const categoryContainer = document.getElementById('categories')
-    categories.forEach((item) => {
-        console.log(item)
-        //create a button
-        const button = document.createElement('button')
-        button.classList = 'btn'
-        button.innerText = item.category
+  const categoryContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    // console.log(item)
+    //create a button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
 
-        //add button to container
-        categoryContainer.appendChild(button)
-    })
-}
+    //add button to container
+    categoryContainer.appendChild(button);
+  });
+};
 
-loadCategories()
+loadCategories();
+loadVideos();
