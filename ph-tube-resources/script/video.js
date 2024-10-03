0ae88a3504ea7b257918474bb284d335509c4a1a
shahidul shohid <shahidulislamshohid7gmail.com>
2024-10-03 21:00:10 +0600

    everything is done

--- ph-tube-resources/script/video.js
+++ ph-tube-resources/script/video.js
@@ -27,7 +27,7 @@ const loadCategories = () => {
     .catch((error) => console.log(error));
 };
 
-const loadVideos = () => {
+const loadVideos = (searchText) => {
   //fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
@@ -165,6 +165,10 @@ const displayCategories = (categories) => {
     categoryContainer.appendChild(buttonContainer);
   });
 };
+
+document.getElementById('search-input').addEventListener('keyup', (e) => {
+  loadVideos(e.target.value)
+})
 
 loadCategories();
 loadVideos();
